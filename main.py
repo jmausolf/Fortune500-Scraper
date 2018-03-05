6080419e2a614d31d31853275496336e7f2212c4
jmausolf <jgm346@gmail.com>
2018-03-05 17:09:12 -0600

    updating to work with python3, write to csv vs. txt

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ import httplib2
 
 h = httplib2.Http(".cache")
 
-with open('fortune500-list.txt', 'w') as f:
+with open('fortune500-list.csv', 'w') as f:
     i = 0
+
+    f.write("rank, company \n")
 
     while i < 500:
         (resp_headers, content) = h.request("http://fortune.com/api/v2/list/2013055/expand/item/ranking/asc/" + str(i)
                                             + "/100", "GET")
 
-        content = json.loads(content)
+        content = json.loads(content.decode('utf-8'))
 
         for company in content["list-items"]:
             i += 1
-            print "[ " + str(i) + " ] - " + company["title"]
+            print ("[ " + str(i) + " ] - " + company["title"])
 
-            f.write(("[ " + str(i) + " ] - " + company["title"] + "\n").encode('utf8'))
+            f.write("{}, {} \n".format(str(i), company["title"]))
