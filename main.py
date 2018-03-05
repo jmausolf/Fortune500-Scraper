c00669a5e00e645e5156a0eb672be2a1770ce22e
jmausolf <jgm346@gmail.com>
2018-03-05 17:28:24 -0600

    removing spaces around f500 col names

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ h = httplib2.Http(".cache")
 with open('fortune500-list.csv', 'w') as f:
     i = 0
 
-    f.write("rank, company \n")
+    f.write("rank,company\n")
 
     while i < 500:
         (resp_headers, content) = h.request("http://fortune.com/api/v2/list/2013055/expand/item/ranking/asc/" + str(i)
